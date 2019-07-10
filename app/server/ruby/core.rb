d089e42e50731fa4001c74fb44ca7d114ffac835
Glen Pike <github@glenpike.co.uk>
2019-07-10 21:11:23 +0100

    Remove vendor Gems that can be installed in system ruby dir.
    ruby-beautify has been modified: https://github.com/samaaron/sonic-pi/issues/928
    so we should leave alone.
    
    Tested starting sonic-pi.  Need to try running tests and translation compile

--- app/server/ruby/core.rb
+++ app/server/ruby/core.rb
@@ -16,15 +16,11 @@
 
 raise "Sonic Pi requires Ruby 2+ to be installed. You are using version #{RUBY_VERSION}" if RUBY_VERSION < "2"
 
-## Ensure native lib dir is available
-require 'rbconfig'
-ruby_api = RbConfig::CONFIG['ruby_version']
-
-
 ## Ensure all libs in vendor directory are available
 Dir["#{File.expand_path("../vendor", __FILE__)}/*/lib/"].each do |vendor_lib|
   $:.unshift vendor_lib
 end
+
 
 begin
   require 'did_you_mean'
@@ -48,32 +44,9 @@ os = case RUBY_PLATFORM
        RUBY_PLATFORM
      end
 
-# special case for proctable lib
-sys_proctable_os = case os
-                   when :raspberry
-                     "linux"
-                   when :linux
-                     "linux"
-                   when :windows
-                     "windows"
-                   when :osx
-                     "darwin"
-                   end
-$:.unshift "#{File.expand_path("../vendor", __FILE__)}/sys-proctable-1.1.3/lib/#{sys_proctable_os}"
-
-
-$:.unshift "#{File.expand_path("../rb-native", __FILE__)}/#{ruby_api}/"
+# $:.unshift "#{File.expand_path("../rb-native", __FILE__)}/#{ruby_api}/"
 
 require 'win32/process' if os == :windows
-
-## Add aubio native library to ENV if not present (the aubio library needs to be told the location)
-native_lib_path = File.expand_path("../../native/", __FILE__)
-ENV["AUBIO_LIB"] ||= Dir[native_lib_path + "/lib/libaubio*.{*dylib,so*,dll}"].first
-
-
-
-
-
 
 module SonicPi
   module Core
